cd54e8d89fa247deb7419a7284698285b79ed6cc
rubin-hood <andreasrubin1980@gmail.com>
2024-10-12 13:30:51 +0200

    update

--- php/kontakt.php
+++ php/kontakt.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars(trim($_POST['name']));
